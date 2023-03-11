a3a7983590eb824f8ec3ae433a76c3a5de54d910
Skorobagach Olga <skorobagach.math@gmail.com>
2023-03-11 13:35:28 +0200

    added home-work-6

---
+++ js/hw6.js
@@ -0,0 +1,27 @@
+/*
+Task#1
+Boolean(Number('10')) + 1;  Поверне: 2;
+'sub ' + Number(false);     Поверне: 'sub 0';
+16  *  '      91    ';      Поверне: 1456;
+true-70;                    Поверне: -69;
+Number(1 + String(1)) + 1;  Поверне: 12;
+*/
+
+// Task#2
+/*
+const numberOfHours = Number(prompt('Задайте кількість годин'));
+const countOfSeconds = numberOfHours * 3600;
+alert(`У ${numberOfHours} годинах ${countOfSeconds} секунд`);
+*/
+
+// Task#3
+/*
+let num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num ++;
+num --;
+alert(num);
+*/
