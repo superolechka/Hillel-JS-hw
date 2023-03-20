87ea62c97f5402acfbae14bb88d55841e696d417
Skorobagach Olga <skorobagach.math@gmail.com>
2023-03-20 13:53:44 +0200

    added home work

---
+++ js/hw22.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let num = prompt('Введіть число');
+while (num === null || !num.trim() || isNaN(+num)) {
+    num = prompt('Спробуйте ще раз. Введіть число:');
+};
+
+function minDivisor(num) {
+    if (Number(num) < 1) {
+        return 'NaN';
+    } else {
+        for (let i = 2; i < Number(num); i++) {
+            if (Number(num) % i === 0) {
+                return i;
+            };
+        }
+        return `${num} - просте число`
+    }
+}
+console.log(minDivisor(num))
