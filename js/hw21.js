6a6b2c813dee79a9199afec94453a70f1cc1c376
Skorobagach Olga <skorobagach.math@gmail.com>
2023-03-20 13:12:11 +0200

    added home work

---
+++ js/hw21.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let num = prompt('Введіть число');
+while (num === null || !num.trim() || isNaN(+num)) {
+        num = prompt('Спробуйте ще раз. Введіть число:');
+};
+
+function isSimpl(num) {
+    let n = 0;
+    for (let i = 1; i <= num; i++) {
+        if (num % i === 0) n += 1;
+    }
+    return n === 2 ? true : false;
+}
+console.log(isSimpl(num))
