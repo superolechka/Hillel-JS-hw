0e4ab861344d5e92f7109e877b877749b600c145
Skorobagach Olga <skorobagach.math@gmail.com>
2023-03-23 12:51:11 +0200

    changed

--- js/hw16.js
+++ js/hw16.js
@@ -4,11 +4,11 @@ const base = prompt('Введіть основу степеня');
 const exponent = prompt('Введіть показник степеня');
 
 function exponentiation( base , exponent = 1 ) {
-    if (!!(+base) == false || isNaN(+exponent)) {
-        return "Some error"} else {
-        const result = Number(base) ** Number(exponent);
-        return result
-    }
+    if (base === null || exponent === null) return "Some error";
+    if (+base === 0 || +exponent === 0) return "Some error";
+    if (isNaN(+base) || isNaN(+exponent)) return "Some error";
+    const result = Number(base) ** Number(exponent);
+    return result
 }
 const power = exponentiation(base, exponent);
 alert(power);
