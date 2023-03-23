8ff3ade4cb4c0689e0dddd237d8550827d7955b8
Skorobagach Olga <skorobagach.math@gmail.com>
2023-03-23 13:03:09 +0200

    changed mistakes

--- js/hw13.js
+++ js/hw13.js
@@ -6,17 +6,22 @@ while (num === null || !num.trim() || isNaN(+num)) {
 };
 let end; 
 const lastFigure = +num % 10;
-switch(lastFigure) {
-    case 1:
-        end = 'рік';
-        break;
-    case 2:
-    case 3:
-    case 4:        
-        end = 'роки';
-        break;
-    default:
-        end = 'років';
-        break;
-}
-alert(`Вам ${num} ${end}.`)
+if (+num === 11 || +num === 12 || +num === 13 || +num === 14 ) {
+    alert(`Вам ${num} років.`)
+} else {
+    switch(lastFigure) {
+        case 1:
+            end = 'рік';
+            break;
+        case 2:
+        case 3:
+        case 4:        
+            end = 'роки';
+            break;
+        default:
+            end = 'років';
+            break;
+    }
+    alert(`Вам ${num} ${end}.`)
+} 
+
