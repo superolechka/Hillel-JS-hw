0735623bed4ca202809ccec78a67426ee6b5fe77
Skorobagach Olga <skorobagach.math@gmail.com>
2023-07-11 14:42:52 +0300

    added hw73

---
+++ js/script.js
@@ -0,0 +1,77 @@
+'use strict'
+
+void function() {
+
+    class Hamburger {
+        constructor(size, stuffing) {
+            this.size = size;
+            this.stuffing = stuffing;
+            this.toppings = [];
+        }
+    
+        static SIZE_SMALL = {
+            price: 50,
+            calories: 20
+        };
+    
+        static SIZE_LARGE = {
+            price: 100,
+            calories: 40
+        };
+    
+        static STUFFING_CHEESE = {
+            price: 10,
+            calories: 20
+        };
+    
+        static STUFFING_SALAD = {
+            price: 20,
+            calories: 5
+        };
+    
+        static STUFFING_POTATO = {
+            price: 15,
+            calories: 10
+        };
+    
+        static TOPPING_MAYO = {
+            price: 20,
+            calories: 5
+        };
+    
+        static TOPPING_SAUCE = {
+            price: 15,
+            calories: 0
+        };
+    
+        addTopping(topping) {
+            this.toppings.push(topping);
+        }
+    
+        calculatePrice() {
+            let totalPrice = this.size.price + this.stuffing.price;
+            this.toppings.forEach((topping) => {
+                totalPrice += topping.price;
+            });
+            return totalPrice;
+        }
+    
+        calculateCalories() {
+            let totalCalories = this.size.calories + this.stuffing.calories;
+            this.toppings.forEach((topping) => {
+                totalCalories += topping.calories;
+            });
+            return totalCalories;
+        }
+    }
+
+    const hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
+    hamburger.addTopping(Hamburger.TOPPING_MAYO);
+
+    console.log(`Calories: ${hamburger.calculateCalories()}`);
+    console.log(`Price: ${hamburger.calculatePrice()}`);
+
+    hamburger.addTopping(Hamburger.TOPPING_SAUCE);
+    console.log(`Price with sauce: ${hamburger.calculatePrice()}`);
+    
+} ()
